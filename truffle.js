dbb22ad3f7b5d20d4ac236f2a54a3b2b9eb34c31
DESKTOP-K1VQ6BV\dscha <dschaberg@gmail.com>
2018-03-16 19:26:07 -0400

    init commit

---
+++ truffle.js
@@ -0,0 +1,23 @@
+module.exports = {
+     // See <http://truffleframework.com/docs/advanced/configuration>
+     // to customize your Truffle configuration!
+     networks: {
+          ganache: {
+               host: "localhost",
+               port: 7545,
+               network_id: "*" // Match any network id
+          },
+          chainskills: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4224",
+            gas: 4700000
+          },
+          rinkeby: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4", //rinkeby test net id
+            gas: 4700000
+          }
+     }
+};
